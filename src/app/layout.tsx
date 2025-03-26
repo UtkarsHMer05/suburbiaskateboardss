29c3eec0b5f206d7b60b78ea8b7f4874aec54d72
Utkarsh Khajuria <157891400+UtkarsHMer05@users.noreply.github.com>
2025-03-27 01:14:58 +0530

    commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Bowlby_One_SC, DM_Mono } from "next/font/google";
 
 import "./globals.css";
 import { Header } from "@/components/Header";
+import { SVGFilters } from "@/components/SVGFilters";
 const bowlby = Bowlby_One_SC({
   subsets: ["latin"],
   display: "swap",
@@ -36,6 +37,7 @@ export default function RootLayout({
           <Header />
           {children}
         </main>
+        <SVGFilters />
       </body>
     </html>
   );
